d91579d963f003644470d330f3c1bc460fcbec73
honeyyusuf <yusufhon0@gmail.com>
2020-11-03 18:31:00 +0000

    with move up

--- client.js
+++ client.js
@@ -9,6 +9,14 @@ const connect = function() {
   conn.on('connect',()=>{
     console.log('Successfully connected to game server');
     conn.write('Name: HY');
+    /*setTimeout(function() {
+      conn.write('Move: up');
+    },1000);
+    setTimeout(function() {
+      conn.write('Move: left');
+    },500); */
+
+    
   });
   conn.on('data',(message)=>{
     console.log(`server says :${message}`);
